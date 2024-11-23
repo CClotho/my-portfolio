04cf889dc02133d96278f90d6333a4ad15d8fa3f
CClotho <124051470+CClotho@users.noreply.github.com>
2024-11-23 15:43:08 +0800

    (added): attribution for jester

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -12,6 +12,11 @@ const Contact = function ({element, inView}: RefType): React.JSX.Element {
                         <img src="/images/alien.png" alt="alien" id="alien"></img>
                         <figcaption> <a href="https://www.freepik.com/icon/alien_6542598">Icon by Freepik</a></figcaption>
                     </figure>
+
+                    <figure  className={` ${inView ? 'show-section' : ''}`}>
+                        <img src="/images/jester-hat.png" alt="jester" id="jester"></img>
+                        <figcaption><a href="https://www.flaticon.com/free-icons/jester-hat" title="jester-hat icons">Jester-hat icons created by iconfield - Flaticon</a></figcaption>
+                    </figure>
                     
                     <div className={`contact-information ${inView ? 'slide-in-active' : ''}`}>
                     <h2> Contact Me: </h2>
